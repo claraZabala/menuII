e870dd60154840811247555cca42e39b8e4bd6a5
clara.zabala <clara.zabala@ocado.com>
2023-05-06 18:39:32 +0200

    p2 bonica però no va

--- p2/p2_ClaraZabala.c
+++ p2/p2_ClaraZabala.c
@@ -49,7 +49,7 @@ void DH(const double x[3], double DHx[2][3]) {
 }
 
 int resoldre(double A[3][3], const double b[3], double x[3]) {
-  double det, cramer[3][3], A_inv[3][3];
+  double det, cramer[3][3];
   det = determinant(A);
   // printf("det=%+.6le\n", det);
   if (fabs(det) < TOL) {
@@ -89,6 +89,10 @@ int resoldre(double A[3][3], const double b[3], double x[3]) {
   return 0;
 }
 
+double norma2(double x[3]) {
+  return sqrt(pow(x[0], 2) + pow(x[1], 2) + pow(x[2], 2));
+}
+
 double determinant(double A[3][3]) {
   return A[0][0] * A[1][1] * A[2][2] + A[0][1] * A[1][2] * A[2][0] +
          A[1][0] * A[2][1] * A[0][2] - A[0][2] * A[1][1] * A[2][0] -
@@ -104,13 +108,15 @@ double determinant(double A[3][3]) {
 int prediccio(int sig, double h, double x[3]) {
   double DHx[2][3], V[3], normaV, cos;
   DH(x, DHx);
-  cos = DHx[0][0] * DHx[1][0] + DHx[0][1] * DHx[1][1] + DHx[0][2] * DHx[1][2];
-  cos /= (norma2(DHx[0]) * norma2(DHx[1]));
+  cos =
+      (DHx[0][0] * DHx[1][0] + DHx[0][1] * DHx[1][1] + DHx[0][2] * DHx[1][2]) /
+      (norma2(DHx[0]) * norma2(DHx[1]));
   printf("pred: cos=%lf\n", cos);
   if (fabs(cos) > TOL) {
-    V[0] = DHx[0][1] * DHx[1][2] - DHx[0][2] * DHx[1][1];
-    V[1] = DHx[0][2] * DHx[1][0] - DHx[0][0] * DHx[1][2];
-    V[2] = DHx[0][0] * DHx[1][1] - DHx[0][1] * DHx[1][0];
+    for (int i = 0; i < 3; i++) {
+      V[i] = DHx[0][(i + 1) % 3] * DHx[1][(i + 2) % 3] -
+             DHx[0][(i + 2) % 3] * DHx[1][(i + 1) % 3];
+    }
     normaV = norma2(V);
     printf("pred: Vt=(%+.2le,%+.2le,%+.2le)\n", V[0] / normaV, V[1] / normaV,
            V[2] / normaV);
@@ -134,6 +140,10 @@ int prediccio(int sig, double h, double x[3]) {
  */
 // x0 conté el punt P inicial, x conté el punt predit i es va corregint
 int correccio(double h, double x0[3], double x[3], int kmax, double prec) {
+  /*
+   * F conté el sistema -H0, -H1, h^2-||x-x0||2^2
+   * DF la derivada del sistema H0, H1, ||x-x0||2^2 - h^2 respecte de x
+   */
   int k = 0;
   double DF[3][3], F[3];
   double Hx[2], DHx[2][3], z[3], err = 1.0;
@@ -144,13 +154,13 @@ int correccio(double h, double x0[3], double x[3], int kmax, double prec) {
       for (int j = 0; j < 3; j++) {
         DF[i][j] = DHx[i][j];
       }
-      F[i] = -1.0 * Hx[i];
+      F[i] = -Hx[i];
     }
     for (int i = 0; i < 3; i++) {
       DF[2][i] = 2.0 * (x[i] - x0[i]);
     }
-    F[2] = -1.0 * (pow(x[0] - x0[0], 2) + pow(x[1] - x0[1], 2) +
-                   pow(x[2] - x0[2], 2) - pow(h, 2));
+    F[2] = pow(h, 2) - pow(x[0] - x0[0], 2) - pow(x[1] - x0[1], 2) -
+           pow(x[2] - x0[2], 2);
     /*printf("DF=(%+.2le,%+.2le,%+.2le)\n(%+.2le,%+.2le,%+.2le)\n"
            "(%+.2le,%+.2le,%+.2le),\n-F=(%+.2le,%+.2le,%+.2le)\n",
            DF[0][0], DF[0][1], DF[0][2], DF[1][0], DF[1][1], DF[1][2], DF[2][0],
@@ -164,7 +174,7 @@ int correccio(double h, double x0[3], double x[3], int kmax, double prec) {
     x[1] += z[1];
     x[2] += z[2];
     err = norma2(z);
-    printf("corr: err=%+.6le\n", err);
+    // printf("corr: err=%+.6le\n", err);
     k++;
   }
   if (k == kmax && err > prec) {
@@ -174,10 +184,6 @@ int correccio(double h, double x0[3], double x[3], int kmax, double prec) {
   printf("corr: k = %d, err = %+.6le, x=(%+.6le, %+.6le, %+.6le)\n", k, err,
          x[0], x[1], x[2]);
   return 0;
-}
-
-double norma2(double x[3]) {
-  return sqrt(pow(x[0], 2) + pow(x[1], 2) + pow(x[2], 2));
 }
 
 int main(void) {
@@ -203,26 +209,22 @@ int main(void) {
    */
   printf("Introdueix un punt inicial x de R3 que sigui de la corba (número + "
          "intro + número + intro + número + intro):\n");
-  fgets(str, 30, stdin);
-  x_aux = strtod(str, &ptr);
-  x[0] = x_aux;
-  fgets(str, 30, stdin);
-  x_aux = strtod(str, &ptr);
-  x[1] = x_aux;
-  fgets(str, 30, stdin);
-  x_aux = strtod(str, &ptr);
-  x[2] = x_aux;
+  for (int j = 0; j < 3; j++) {
+    fgets(str, 30, stdin);
+    x_aux = strtod(str, &ptr);
+    x[j] = x_aux;
+  }
   if (errno == EINVAL) {
     printf("x introduït incorrectament!");
     return 1;
   }
+  printf("Has introduit el punt (%lf, %lf, %lf)\n", x[0], x[1], x[2]);
   H(x, Hx);
   printf("Hx = (%lf, %lf)\n", Hx[0], Hx[1]);
   if (Hx[0] > prec || Hx[1] > prec) {
     printf("El x introduït no és de la corba\n");
     return -1;
   }
-  printf("Has introduit el punt (%lf, %lf, %lf)\n", x[0], x[1], x[2]);
   fprintf(fitxer, "%d %+.6le %+.6le %+.6le\n", i, x[0], x[1], x[2]);
   printf("Introdueix un valor sig que sigui 1 o -1: ");
   fgets(str, 30, stdin);
